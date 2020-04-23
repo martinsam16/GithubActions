05989f226ea22fc12019bc9149bf60003602acdb
MartinSamanArata2018 <martin.saman@vallegrande.edu.pe>
2020-04-23 11:13:54 -0500

    :fire: funciona secrets config

--- hola.py
+++ hola.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hola():
-    return "Hola mundo probando cambios :DDD"
+    return "Hola mundo CI/CD :DD"
 
 
 if __name__ == '__main__':
